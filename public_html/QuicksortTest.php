d9bdf331e644e0b886b6e7b764a63105edeec947
Karasar, Umut <umut.karasar@sony.com>
2018-02-21 11:04:30 +0300

    master Refactoring for unit test

--- public_html/QuicksortTest.php
+++ public_html/QuicksortTest.php
@@ -7,22 +7,19 @@
  */
 
 use PHPUnit\Framework\TestCase;
+
 require_once 'vendor/autoload.php';
 require_once __DIR__ . '/quicksort.php';
 
+class QuicksortTest extends TestCase {
 
-    class QuicksortTest extends TestCase{
-
-    public function firstTest($input){
-        $Quick_Sort = new QuickSort($input);
-        $input =  array(array(1, 9, 0, 3),array(7, 2));
-        $result = $Quick_Sort->quickSort($input);
-        $this->assertEquals(array(array(0, 1, 3, 9), array(2,7)), $result);
+    public function firstTest(){
+        $quickSort = new QuickSort();
+        $input =  [ 1, 9, 0, 3 ];
+        $result = $quickSort->apply($input);
+        $this->assertEquals([0, 1, 3, 9], $result);
     }
-
-
-
 }
 
 $test = new QuicksortTest();
-$test->firsttest($input);
+$test->firstTest();
--- public_html/quicksort.php
+++ public_html/quicksort.php
@@ -7,29 +7,26 @@
  * Time: 16:28
  */
 
-
+/**
+ * This is quick sort algorithm function. Function takes unsorted parameter to get the unsorted set of numbers.
+ * It firstly controls count of numbers. If there is only one number, no need to sort so it returns what it is the number.
+ * If there are more than one number, quick sort algorithm starts. Algorithm choose a number and accept it the pivot
+ * number. I preferred first number as pivot with using $unsorted[]. Two array defined left and right. Left  for
+ * smaller than pivot, right for bigger than pivot. All elements of the array compared pivot value with for loop. The
+ * smaller ones added left array, bigger ones added right array. The left and right arrays call to function to be sort
+ * with same logic. Latest all arrays merged and returned.
+ *
+ */
 class QuickSort {
-
-    /* This is quick sort algorithm function. Function takes unsorted parameter to get the unsorted set of numbers.
-    It firstly controls count of numbers. If there is only one number, no need to sort so it returns what it is the number.
-
-    If there are more than one number, quick sort algorithm starts. Algorithm choose a number and accept it the pivot
-    number. I preferred first number as pivot with using $unsorted[]. Two array defined left and right. Left  for
-    smaller than pivot, right for bigger than pivot. All elements of the array compared pivot value with for loop. The
-    smaller ones added left array, bigger ones added right array. The left and right arrays call to function to be sort
-    with same logic. Latest all arrays merged and returned.
-      */
-
 
     /**
      * QuickSort number sorting algorithm
-     * @param $unsorted
-     * @return int
+     *
+     * @param array $unsorted
+     * @return array
      */
-
-    public function quickSort($unsorted)
+    public function apply($unsorted)
     {
-
         if (count($unsorted) <= 1) {
             return $unsorted;
         } else {
@@ -43,17 +40,7 @@ class QuickSort {
                     $right[] = $unsorted[$i];
                 }
             }
-            return array_merge($this->quickSort($left), array($pivot), $this->quickSort($right));
+            return array_merge($this->apply($left), array($pivot), $this->apply($right));
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
