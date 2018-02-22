9d461f562f973ad78df864b19a863f9dcff6f58b
Onur Değerli <Onur.Degerli@eu.sony.com>
2018-02-22 11:19:43 +0300

    Adding composer autolaoder

--- web/index.php
+++ web/index.php
@@ -1,10 +1,24 @@
 <?php
-function custom_autoloader( $class ) {
-    include $class . '.php';
-}
 
-// register the autoloader
-spl_autoload_register( 'custom_autoloader' );
+$type = 'with_composer';
+
+switch ( $type ) {
+    case 'with_custom_autoloder':
+        function custom_autoloader( $class ) {
+            include 'classes/' . $class . '.php';
+        }
+
+        // register the autoloader
+        spl_autoload_register( 'custom_autoloader' );
+    break;
+
+    case 'with_composer':
+        require __DIR__ . '/vendor/autoload.php';
+    break;
+    default:
+    # code...
+    break;
+}
 
 $quickSort = new QuickSort();
 $sorted    = $quickSort->apply( [3,7,1,5] );
