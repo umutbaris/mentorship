17e129c8d57011ef13388a798ed34d2d2c0dee6d
Onur Değerli <Onur.Degerli@eu.sony.com>
2018-02-22 08:20:10 +0300

    Adding custom autoloading method

--- web/index.php
+++ web/index.php
@@ -1,57 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: trkarasu
- * Date: 29/01/2018
- * Time: 21:37
- */
-
-
-
-require_once __DIR__ . '/quicksort.php';
-
-class Test
-{
-    /**
-     * Test constructor.
-     */
-
-    function __construct()
-    {
-
-    }
-
-    /* This function wrote to test quicksort algorithm. QuickSort class assigned Quick_Sort object. There is a multiple
-    array for testing. All elements of the array calls quick sort algorithm which foreach loop. So quicksort algorithm
-    returns sorted set of numbers. Then printed sorted numbers.   */
-
-    /**
-     * Test values for any sorting algorithm
-     * @param $unsorted
-     * @return int
-     */
-
-    public function applyTest($unsorted){
-
-        $Quick_Sort = new QuickSort($unsorted);
-        $unsorted = array
-        (
-            array(1, 9, 0, 3),
-            array(6, 6, 9, 4, 9, 1, 1, 7, 7, 6, 6),
-            array(3, 5, 0),
-            array(5, 8, 0, 6, 3, 5, 3, 4),
-            array(4, 4, 4, 4, 4, 4,)
-        );
-
-
-        foreach ($unsorted as $val)
-        {
-            $sorted = $Quick_Sort->quickSort($val);
-            echo implode(" - ", $sorted), "<br>";
-        }
-    }
-
+function custom_autoloader( $class ) {
+    include $class . '.php';
 }
 
-$test = new Test();
-$test->applyTest($unsorted);
+// register the autoloader
+spl_autoload_register( 'custom_autoloader' );
+
+$quickSort = new QuickSort();
+$sorted    = $quickSort->apply( [3,7,1,5] );
+print_r($sorted);
+
+// check the list of all loaded files
+var_dump( get_included_files() );
