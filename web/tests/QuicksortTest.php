616830f07ab612122b33953bd82067578a6ed989
Onur Değerli <Onur.Degerli@eu.sony.com>
2018-02-28 17:04:09 +0300

    Running phpunits

--- web/tests/QuicksortTest.php
+++ web/tests/QuicksortTest.php
@@ -8,18 +8,12 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once 'vendor/autoload.php';
-require_once __DIR__ . '/quicksort.php';
-
 class QuicksortTest extends TestCase {
 
-    public function firstTest(){
-        $quickSort = new QuickSort();
-        $input =  [ 1, 9, 0, 3 ];
-        $result = $quickSort->apply($input);
-        $this->assertEquals([0, 1, 3, 9], $result);
-    }
+	public function testApply() {
+		$quickSort = new QuickSort();
+		$input     = [ 1, 9, 0, 3 ];
+		$result    = $quickSort->apply( $input );
+		$this->assertEquals( [0, 1, 3, 9], $result );
+	}
 }
-
-$test = new QuicksortTest();
-$test->firstTest();
